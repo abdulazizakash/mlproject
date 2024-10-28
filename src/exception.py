15a11188e3d10d1ea208723e69b6e027b8756a54
Abdul Aziz Akash <crysisofakash99@gmail.com>
2024-10-28 15:38:36 +0600

    EDA and Problems Statement

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,5 @@
 import sys
-from src.logger import logging
-
+from logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -22,6 +21,7 @@ class CustomException(Exception):
     
 
 # if __name__=="__main__":
+    
 #     try:
 #         a=1/0
 #     except Exception as e:
